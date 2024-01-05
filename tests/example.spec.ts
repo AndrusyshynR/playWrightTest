7701ddc044c84248feb2cae1f9cbb81329804b34
AndrusyshynR <romanow3333@gmail.com>
2024-01-05 13:30:04 +0200

    refactoring code

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,66 +1,39 @@
 import { test, expect } from '@playwright/test';
 import { log } from 'console';
 
-// test.beforeEach( async ({page}) => {
-//   await page.goto('https://carsandbids.com/')
-// });
-
-// test('Have Title', async ({ page }) => {
-//   await expect(page).toHaveTitle(/Cars & Bids: Auctions of cool modern cars, trucks, and SUVs/);
-// });
-
-// test('go sites and click', async ({ page }) => {
-//   await page.getByRole('link', { name: 'Sell a Car'}).click();
-// });
-
-// test('go new page', async ({ page }) => {
-//   await page.goto('https://carsandbids.com/sell-car/?utm_source=nav&utm_medium=site&utm_campaign=sell&utm_content=header');
-//   await page.getByRole('link', { name: 'Sign Up'}).click();
-// });
-
-
-
-
-// test('click button', async ({ page }) => {
-//   await page.getByRole('link', { name: 'Get Started Now'}).click();
-
-// // Expects page to have a heading with the name of Installation.
-//   // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
-
-//======================================== MONO ==============================================================
-
-// test.beforeEach( async ({page}) => {
-//   await page.goto('https://merch.mono.st4g3.com/')
-// });
+//======================================== Merch ========================================
 
 test('go pages merch', async ({ page }) => {
   await page.goto('https://merch.mono.st4g3.com/');
-  await expect(page).toHaveTitle(/Мерч від monobank | Головна/);
-  await page.getByRole('link', { name: 'product-image Футболка FOR'}).click();
-});
+  await page.getByRole('link', { name: 'product-image Футболка MONO'}).click();
 
-test('go new page', async ({ page }) => {
-  await page.goto('https://merch.mono.st4g3.com/product/1');
-  await page.getByRole('button', { name: 'Додати до кошика'}).click({ timeout: 60000 });
+  await page.goto('https://merch.mono.st4g3.com/product/3');
+  await page.getByRole('button', { name: 'Додати до кошика'}).click();
   await page.getByRole('banner').getByRole('img').first().click();
-});
 
-//test('go sale', async ({ page }) => {
-  //await page.locator('.header-btn-wrapper').click();
-  //await page.getByRole('banner').getByRole('img').first().click();
-  //await page.getByRole('main').getByRole('button', { name: 'Перейти до кошика' }).click()
-//});
-
-test('go page cart', async ({ page }) => {
   await page.goto('https://merch.mono.st4g3.com/personal/cart');
   await page.getByRole('button', { name: 'Оформити замовлення' }).click();
 });
 
-// test('error', async ({ page }) => {
-//   await page.getByRole('button', { name: 'Оформити замовлення' }).click();
-// });
-
-test('go page checkout', async ({ page }) => {
+//authorization
+test('authorization checkout', async ({ page }) => {
   await page.goto('https://checkout.mono.st4g3.com/order/ab7c98cb-8970-4323-98f4-0bce26f76d6e/auth')
+  await page.getByLabel('Номер телефону').click();
+  await page.getByLabel('Номер телефону').fill('+38 (063) 171 72 23');
+  await page.getByRole('button', { name: 'Продовжити' }).click();
+
+  await page.goto('https://checkout.mono.t3zt.com/order/224d1265-ac52-4351-a7cc-866af16f28ea/verification')
+  await page.getByRole('textbox').first().click();
+  await page.getByRole('textbox').first().fill('0');
+  await page.getByRole('textbox').nth(1).fill('0');
+  await page.getByRole('textbox').nth(2).fill('0');
+  await page.getByRole('textbox').nth(3).fill('0');
 });
+
+//======================================== Checkout ========================================
+
+// test('checkout', async ({ page }) => {
+//   await page.goto('https://checkout.mono.t3zt.com/order/0396e737-b1db-4868-9faa-af44d6d6039e');
+//   await page.getByRole('button', { name: 'Оплатити замовлення До сплати: 320 ₴' }).click();
+//   await page.getByRole('button', { name: 'Підтвердити' }).click();
+// });
